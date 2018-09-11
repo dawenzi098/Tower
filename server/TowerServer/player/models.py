1dfb0bdaf59bfe5455e20ed2f1268c1a68cdb25b
zxf20180725 <zhaoxiongfei6789@163.com>
2018-09-11 17:04:56 +0800

    新增：django服务端初始结构

---
+++ server/TowerServer/player/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Player(models.Model):
+    username = models.CharField(max_length=32, null=False, verbose_name="账号")
+    password = models.CharField(max_length=32, null=False, verbose_name="密码")
+    email = models.EmailField(null=False, verbose_name="电子邮箱")
+
+    nickname = models.CharField(max_length=16, null=False, verbose_name="昵称")
+    hp = models.PositiveIntegerField(null=False, default=0, verbose_name="生命值")
+    atk = models.PositiveIntegerField(null=False, default=0, verbose_name="攻击力")
+    defense = models.PositiveIntegerField(null=False, default=0, verbose_name="防御力")
+    coin = models.PositiveIntegerField(null=False, default=0, verbose_name="金币")
---
+++ server/TowerServer/player/views.py
@@ -0,0 +1,8 @@
+
+# Create your views here.
+from rest_framework.views import APIView
+
+
+class RegisterView(APIView):
+    def get(self, request, *args, **kwargs):
+        pass
