c4b9b527bc341030e85ae011044bf610041749b7
zxf20180725 <zhaoxiongfei6789@163.com>
2018-09-14 10:53:49 +0800

    新增：地图编辑器开发中

--- client/core/logic/edit_map.py
+++ client/core/logic/edit_map.py
@@ -17,10 +17,10 @@ class Panel(BasePanel):
     地板面板
     """
 
-    def __init__(self, t, w, h):
-        super().__init__(500, 0, w, h)
+    def __init__(self, t, x, y, w, h):
+        super().__init__(x, y, w, h)
         self.focus = -1
-        self.t = t  # 类型    0：地板
+        self.t = t  # 类型    0：地板    1：墙
 
     def mouse_move(self, x, y):
         if not self.mouse_in_panel(x, y):
@@ -33,18 +33,27 @@ class Panel(BasePanel):
         if not self.mouse_in_panel(x, y):
             self.focus = -1  # 鼠标不在面板范围内
             return
+
+        global current_tile
         if self.t == 0:  # 地板面板
-            global current_tile
             current_tile = self.focus + 1000
+        elif self.t == 1:  # 墙面板
+            current_tile = self.focus + 2000
+        print(current_tile)
 
     def draw_focus(self):
         x = self.x + (self.focus % 4) * 32
         y = self.y + (self.focus // 4) * 32
-        pygame.draw.rect(g.screen, Color(255, 0, 0), (x, y, 32, 32), 1)
+        if self.focus != -1:
+            pygame.draw.rect(g.screen, Color(255, 0, 0), (x, y, 32, 32), 1)
 
     def draw(self):
         # 画图块
-        g.screen.blit(g.surface_pool[2], (self.x, self.y))
+        if self.t == 0:
+            g.screen.blit(g.surface_pool[2], (self.x, self.y))
+        elif self.t == 1:
+            g.screen.blit(g.surface_pool[3], (self.x, self.y))
+
         # 画焦点线框
         self.draw_focus()
 
@@ -73,6 +82,8 @@ class MapPanel(BasePanel):
         # dx, dy = self.get_dxy(x, y)
         if 1000 <= current_tile < 2000:
             current_map.bottom[current_map.current_level][self.index_x][self.index_y] = current_tile
+        elif 2000 <= current_tile < 3000:
+            current_map.top[current_map.current_level][self.index_x][self.index_y] = current_tile
 
     def draw_focus(self):
         x = self.x + self.index_x * 32
@@ -80,19 +91,27 @@ class MapPanel(BasePanel):
         pygame.draw.rect(g.screen, Color(0, 255, 0), (x, y, 32, 32), 1)
 
     def draw(self):
-        # 画焦点线框
-        self.draw_focus()
         # 画地图
         for y in range(13):
             for x in range(13):
-                tile_val = current_map.bottom[current_map.current_level][x][y]
-                if 1000 <= tile_val < 2000:
+                btm_tile_val = current_map.bottom[current_map.current_level][x][y]
+                top_tile_val = current_map.top[current_map.current_level][x][y]
+                if 1000 <= btm_tile_val < 2000:  # 画底层
                     g.screen.blit(g.surface_pool[2], (self.x + x * 32, self.y + y * 32),
-                                  ((tile_val - 1000) % 4 * 32, (tile_val - 1000) // 4 * 32, 32, 32))
+                                  ((btm_tile_val - 1000) % 4 * 32, (btm_tile_val - 1000) // 4 * 32, 32, 32))
+                if 2000 <= top_tile_val < 3000:  # 画墙
+                    g.screen.blit(g.surface_pool[3], (self.x + x * 32, self.y + y * 32),
+                                  ((top_tile_val - 2000) % 4 * 32, (top_tile_val - 2000) // 4 * 32, 32, 32))
+
+        # 画焦点线框
+        self.draw_focus()
 
 
 # 创建地板面板
-floor_panel = Panel(0, 128, 64)
+floor_panel = Panel(0, 500, 0, 128, 64)
+# 创建墙
+wall_panel = Panel(1, 500, 100, 128, 32)
+
 # 创建地图面板
 map_panel = MapPanel()
 
@@ -102,10 +121,12 @@ def event_handler(event):
     if event.type == pygame.MOUSEMOTION:
         x, y = pygame.mouse.get_pos()
         floor_panel.mouse_move(x, y)
+        wall_panel.mouse_move(x, y)
         map_panel.mouse_move(x, y)
     elif event.type == pygame.MOUSEBUTTONDOWN:
         x, y = pygame.mouse.get_pos()
         floor_panel.mouse_down(x, y)
+        wall_panel.mouse_down(x, y)
         map_panel.mouse_down(x, y)
 
 
@@ -123,8 +144,10 @@ def draw():
 
     # 绘制地板图块
     floor_panel.draw()
+    # 绘制墙面板
+    wall_panel.draw()
     # 绘制墙图块
-    g.screen.blit(g.surface_pool[3], (500, 100))
+    # g.screen.blit(g.surface_pool[3], (500, 100))
     # 绘制宝石图块
     g.screen.blit(g.surface_pool[4], (650, 0))
     # 绘制门图块
