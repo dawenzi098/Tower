6c9332c7c44a194ee7c67d347d579b9c9eb5a758
zxf20180725 <zhaoxiongfei6789@163.com>
2018-09-19 13:55:55 +0800

    新增：服务端新增塔的相关model

--- server/TowerServer/player/admin.py
+++ server/TowerServer/player/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from player.models import Player
+from player.models import Player, TowerPlayer, Tower
 
 
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('nickname', 'coin', 'post_date')
 
 
+class TowerAdmin(admin.ModelAdmin):
+    list_display = ('player', 'post_date')
+
+
+class TowerPlayerAdmin(admin.ModelAdmin):
+    list_display = ('player', 'level', 'hp', 'atk', 'defense')
+
+
 admin.site.register(Player, PlayerAdmin)
+admin.site.register(Tower, TowerAdmin)
+admin.site.register(TowerPlayer, TowerPlayerAdmin)
--- server/TowerServer/player/models.py
+++ server/TowerServer/player/models.py
@@ -43,3 +43,37 @@ class Player(models.Model):
     class Meta:
         verbose_name = "玩家"
         verbose_name_plural = verbose_name
+
+
+class Tower(models.Model):
+    player = models.ForeignKey('Player', null=False, on_delete=models.CASCADE, verbose_name='建造者')
+    data = models.TextField(null=False, default="{'level':0,'bottom':None,'top':None}", verbose_name="地图数据")
+    post_date = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    def __str__(self):
+        return self.player.nickname
+
+    class Meta:
+        verbose_name = "塔"
+        verbose_name_plural = verbose_name
+
+
+class TowerPlayer(models.Model):
+    """
+    玩家在塔中的数据（存档）
+    """
+    player = models.ForeignKey('Player', null=False, on_delete=models.CASCADE, verbose_name='玩家')
+    hp = models.PositiveIntegerField(null=False, default=0, verbose_name="生命值加成")
+    atk = models.PositiveIntegerField(null=False, default=0, verbose_name="攻击力加成")
+    defense = models.PositiveIntegerField(null=False, default=0, verbose_name="防御力加成")
+    tower = models.ForeignKey('Tower', null=False, on_delete=models.CASCADE, verbose_name='塔')
+    x = models.PositiveSmallIntegerField(null=False, default=0, verbose_name="横坐标")
+    y = models.PositiveSmallIntegerField(null=False, default=0, verbose_name="纵坐标")
+    level = models.PositiveSmallIntegerField(null=False, default=1, verbose_name="当前层数")
+
+    def __str__(self):
+        return self.player.nickname
+
+    class Meta:
+        verbose_name = "玩家属性"
+        verbose_name_plural = verbose_name
